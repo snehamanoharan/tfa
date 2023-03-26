ccba4961835d3171cf04ec3235c83e30525867c7
snehamanoharan <59600528+snehamanoharan@users.noreply.github.com>
2023-03-26 21:57:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region ="${var.region}"
-access_key = "AKIATRT7RKWAVKNZGKH3"
-secret_key = "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"
+access_key = "${var.access_key}"
+secret_key = "${var.secret.key}"
 }
