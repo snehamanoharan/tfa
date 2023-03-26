8c4a61841cac3fbc33f6ab1a2ce25dcb15322a83
SnehaManoharan <snehamanoharan992@gmail.com>
2023-03-26 21:34:57 +0530

    --help

--- output.tf
+++ output.tf
@@ -1,4 +1,8 @@
-output "rsrcdetails" {
-    insta=aws_instance.inst.id
-    bucket=aws_s3_bucket.buck.id
+output "inst_id" {
+    value=aws_instance.inst.id
+    
+}
+
+output "s3_id"{
+    value=aws_s3_bucket.buck.id
 }
